3dc3de5c05cd25b50f451d427847fd0b7a3caf9c
Jiri Kvita <jiri.kvita@gmail.com>
2023-07-15 18:04:53 +0200

    calibration back

--- python/fitToF.py
+++ python/fitToF.py
@@ -16,21 +16,27 @@ def PrintUsage(argv):
     print('{} histos/output_list_root_run_XYZ_plots.root [-b]'.format(argv[0]))
     return
 
+##########################################
 
 def Fit(h, tag, ct, w, t1, t2, peaksf = 1.):
     fname = 'fit{}'.format(tag)
     hname = h.GetName()
     fit = ROOT.TF1(fname, '[0]*exp(-(x-[1])^2/(2*[2]^2))', t1, t2)
     #ampl = h.GetMaximum() / peaksf
-    ampl = h.GetBinContent(h.FindBin(ct))
+    ampl = h.GetBinContent(h.FindBin(ct)) / peaksf
     print('Amplitude initially ', ampl)
     fit.SetParameters(ampl, ct, w)
-    h.Fit(fname, 'q', '')
+    fit.SetParLimits(2, 0., 1.7)
+    for ip in range(0, fit.GetNpar()):
+        print(fit.GetParameter(ip))
+    #prefit = fit.DrawCopy('same')
+    #stuff.append(prefit)
+    h.Fit(fname, '', '')
     mean = fit.GetParameter(1)
     sigma = fit.GetParameter(2)
     #print(f'1) {hname } mean={mean:1.3f} ns; sigma={sigma:1.3f} ns')
     sf = 2.
-    h.Fit(fname, 'q', '', mean - sf*sigma, mean + sf*sigma)
+    #h.Fit(fname, '', '', mean - sf*sigma, mean + sf*sigma)
     mean = fit.GetParameter(1)
     sigma = fit.GetParameter(2)
     print(f'{hname } mean={mean:1.3f} ns; sigma={sigma:1.3f} ns')
@@ -40,6 +46,7 @@ def Fit(h, tag, ct, w, t1, t2, peaksf = 1.):
     return fit
 
 
+##########################################
 
 
 
@@ -108,17 +115,21 @@ def main(argv):
     ROOT.gStyle.SetOptStat(0)
     
     hTOFAll.SetLineColor(ROOT.kBlack)
-    hTOFAll.SetLineWidth(2)
-    hTOFAll.Draw('hist')
+    hTOFAll.SetMarkerColor(hTOFAll.GetLineColor())
+    hTOFAll.SetMarkerStyle(20)
+    hTOFAll.SetMarkerSize(1)
+    hTOFAll.SetLineWidth(1)
+    hTOFAll.Draw('e1')
 
     # def Fit(h, tag, ct, w, t1, t2, peaksf = 1.):
 
     off = 0.
     if 'uncalibrated' in inFile.GetName():
         off = 3.
-    fite = Fit(hTOFAll, '_el', 11.7 + off, 1., 11. + off, 13. + off, 1.)
-    fitp = Fit(hTOFAll, '_p',  16. + off, 1., 14 + off,  17.5 + off, 1.)
-    #fitd = Fit(hTOFAll, '_d',  25., 1., 24, 26, 1.)
+    width = 0.2
+    fite = Fit(hTOFAll, '_el', 11.7 + off, width, 11. + off, 13. + off, 1.)
+    fitp = Fit(hTOFAll, '_p',  16. + off, width, 14 + off,  17.5 + off, 1.)
+    fitd = Fit(hTOFAll, '_d',  25. + off, 0.8, 24.3 + off, 25.7 + off, 0.8)
 
     tdif_e_p = fitp.GetParameter(1) - fite.GetParameter(1)
     tex = ROOT.TLatex(0.7, 0.8, 't_{p}-t_{e}=' + '{:1.2f}'.format(tdif_e_p))
@@ -126,17 +137,17 @@ def main(argv):
     tex.Draw()
     stuff.append(tex)
 
-    #tdif_e_d = fitd.GetParameter(1) - fite.GetParameter(1)
-    #tex = ROOT.TLatex(0.7, 0.7, 't_{d}-t_{e}=' + '{:1.2f}'.format(tdif_e_d))
-    #tex.SetNDC()
-    #tex.Draw()
-    #stuff.append(tex)
+    tdif_e_d = fitd.GetParameter(1) - fite.GetParameter(1)
+    tex = ROOT.TLatex(0.7, 0.7, 't_{d}-t_{e}=' + '{:1.2f}'.format(tdif_e_d))
+    tex.SetNDC()
+    tex.Draw()
+    stuff.append(tex)
 
 
     
-    hTOFEl.SetLineColor(ROOT.kRed)
-    hTOFEl.SetLineWidth(2)
-    hTOFEl.Draw('hist same')
+    #hTOFEl.SetLineColor(ROOT.kRed)
+    #hTOFEl.SetLineWidth(2)
+    #hTOFEl.Draw('hist same')
 
     ROOT.gPad.SetLogy(1)
     ROOT.gPad.Update()
