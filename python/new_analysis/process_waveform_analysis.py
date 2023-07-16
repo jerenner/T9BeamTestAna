da14c3d537207b769538d248e98fb8b7b79dcba8
Nick Prouse <n.prouse@imperial.ac.uk>
2023-07-16 18:30:31 +0200

    Allow processing multiple input files to one output file

--- python/new_analysis/process_waveform_analysis.py
+++ python/new_analysis/process_waveform_analysis.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 #
 # Process waveform analysis of root file
-#
+# Usage:
+#    python3 process_waveform_analysis.py [input_files] [config_file] [output_file]
+#        input_file, input_file2, ...: any number of root files from midas2root
+#        config_file: config JSON file
+#        output_file: output ntuple root file
 
-import sys, warnings
+import sys
 
 import numpy as np
 import uproot as ur
@@ -11,26 +15,41 @@ import json5
 from waveform_analysis import WaveformAnalysis
 
 
-def PrintUsage(argv):
+def print_usage(argv):
     print('Usage:')
-    print(f"{argv[0]} input_file config_file output_file")
+    print(f"{argv[0]} input_file [input_file2 ...] config_file output_file")
+    print(f"    input_file, input_file2, ...: any number of root files from midas2root")
+    print(f"    config_file: config JSON file")
+    print(f"    output_file: output ntuple root file")
     return
 
 
 def process_waveforms(argv):
     if len(argv) < 4:
-        PrintUsage(argv)
+        print("Not enough arguments provided.")
+        print_usage(argv)
         return
 
-    root_filename = argv[1]
-    config_filename = argv[2]
-    output_filename = argv[3]
-    print(f"Processing root file {root_filename} using config file {config_filename}")
+    root_filenames = argv[1:-2]
+    config_filename = argv[-2]
+    output_filename = argv[-1]
+
+    print(f"Using config file {config_filename}")
     with open(config_filename) as file:
         config = json5.load(file)["WaveAnalysis"]
+
+    output_file = ur.recreate(output_filename)
+    print(f"{len(root_filenames)} input root files to process.")
+    for f in root_filenames:
+        process_file(f, config, output_file)
+    output_file.close()
+
+
+def process_file(root_filename, config, output_file):
+    print(f"Processing root file {root_filename}")
     run_file = ur.open(root_filename)
     digitizers = ["midas_data_D300", "midas_data_D301", "midas_data_D302", "midas_data_D303"]
-    channels_per_digitizer = config["NumberOfChannels"]//len(digitizers)
+    channels_per_digitizer = config["NumberOfChannels"] // len(digitizers)
     channels = []
     for c in range(config["NumberOfChannels"]):
         channel = f"{digitizers[c // channels_per_digitizer]}/Channel{c % channels_per_digitizer}"
@@ -39,7 +58,8 @@ def process_waveforms(argv):
         print(f"{channel} is {'active' if config['ActiveChannels'][c] else 'not active'}")
     events_per_channel = [run_file[c].num_entries for c in channels]
     if min(events_per_channel) != max(events_per_channel):
-        raise ValueError(f"Channels have different numbers of events: {','.join([f'{c}:{n}' for c, n in zip(channels, events_per_channel)])}")
+        raise ValueError(
+            f"Channels have different numbers of events: {','.join([f'{c}:{n}' for c, n in zip(channels, events_per_channel)])}")
     n_channels = len(channels)
     n_events = events_per_channel[0]
     print(f"All {n_channels} active channels have {n_events} events")
@@ -50,14 +70,15 @@ def process_waveforms(argv):
     peak_times = np.zeros((n_events, n_channels, n_peaks))
     signal_times = np.zeros((n_events, n_channels, n_peaks))
     integrated_charges = np.zeros((n_events, n_channels, n_peaks))
-
     for i, c in enumerate(channels):
         print(f"Processing {c}")
         waveforms = run_file[c].array()
         waveform_analysis = WaveformAnalysis(waveforms,
                                              threshold=config["Thresholds"][i],
-                                             analysis_window=(config["AnalysisWindowLow"][i], config["AnalysisWindowHigh"][i]),
-                                             pedestal_window=(config["PedestalWindowLow"][i], config["PedestalWindowHigh"][i]),
+                                             analysis_window=(
+                                             config["AnalysisWindowLow"][i], config["AnalysisWindowHigh"][i]),
+                                             pedestal_window=(
+                                             config["PedestalWindowLow"][i], config["PedestalWindowHigh"][i]),
                                              reverse_polarity=(config["Polarity"][i] == 0),
                                              voltage_scale=config["VoltageScale"])
         waveform_analysis.run_analysis()
@@ -67,19 +88,22 @@ def process_waveforms(argv):
         peak_times[:, i] = waveform_analysis.peak_times
         signal_times[:, i] = waveform_analysis.signal_times
         integrated_charges[:, i] = waveform_analysis.integrated_charges
+    run_file.close()
 
-    print(f"Writing to {output_filename}")
-    output_file = ur.recreate(output_filename)
-    output_file["anaTree"] = {
-        "nChannels": np.repeat(n_channels, n_events),
-        "Pedestal": pedestals,
-        "PedestalSigma": pedestal_sigmas,
-        "PeakVoltage": peak_voltages,
-        "PeakTime": peak_times,
-        "SignalTime": signal_times,
-        "IntCharge": integrated_charges
-    }
-    output_file.close()
+    print(f"Writing to {output_file.file_path}")
+    branches = {
+            "nChannels": np.repeat(n_channels, n_events),
+            "Pedestal": pedestals,
+            "PedestalSigma": pedestal_sigmas,
+            "PeakVoltage": peak_voltages,
+            "PeakTime": peak_times,
+            "SignalTime": signal_times,
+            "IntCharge": integrated_charges
+        }
+    if "anaTree" not in output_file.keys():
+        output_file["anaTree"] = branches
+    else:
+        output_file["anaTree"].extend(branches)
 
 
 if __name__ == "__main__":
