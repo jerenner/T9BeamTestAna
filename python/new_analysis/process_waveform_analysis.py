7a1180d93820d068dbac18d47b5c2f4e58b408ea
Nick Prouse <n.prouse@imperial.ac.uk>
2023-07-29 13:27:10 +0100

    Add optional digitizer-level branches (timestamp and trigger time) to pass through from root file when making ntuples

--- python/new_analysis/process_waveform_analysis.py
+++ python/new_analysis/process_waveform_analysis.py
@@ -52,25 +52,31 @@ def process_file(root_filename, config, output_file):
     run_file = ur.open(root_filename)
     digitizers = ["midas_data_D300", "midas_data_D301", "midas_data_D302", "midas_data_D303"]
     channels_per_digitizer = config["NumberOfChannels"] // len(digitizers)
-    channels = []
+    digitizer_channels = {d: [] for d in digitizers}
     for c in range(config["NumberOfChannels"]):
-        channel = f"{digitizers[c // channels_per_digitizer]}/Channel{c % channels_per_digitizer}"
+        channel = f"Channel{c % channels_per_digitizer}"
+        digitizer = digitizers[c // channels_per_digitizer]
         if config["ActiveChannels"][c]:
-            channels.append(f"{channel}")
-        print(f"{channel} is {'active' if config['ActiveChannels'][c] else 'not active'}")
-    events_per_channel = [run_file[c].num_entries for c in channels]
-    if min(events_per_channel) != max(events_per_channel):
-        raise ValueError(f"Channels have different numbers of events: {','.join([f'{c}:{n}' for c, n in zip(channels, events_per_channel)])}")
-    n_channels = len(channels)
-    n_events = events_per_channel[0]
+            digitizer_channels[digitizer].append(f"{channel}")
+        print(f"{digitizer}/{channel} is {'active' if config['ActiveChannels'][c] else 'not active'}")
+    active_digitizers = [d for d in digitizers if len(digitizer_channels[d]) > 0]
+    events_per_digitizer = [run_file[d].num_entries for d in active_digitizers]
+    if min(events_per_digitizer) != max(events_per_digitizer):
+        raise ValueError("Digitizers wth active channels have different numbers of events: " +
+                         ', '.join([f'{d}:{n}' for d, n in zip(active_digitizers, events_per_digitizer)]))
+    n_channels = sum(len(channels) for channels in digitizer_channels.values())
+    n_events = events_per_digitizer[0]
     if n_events == 0:
-        print(f"WARNING: The channels in {root_filename} have zero events. Skipping this file.")
+        print(f"WARNING: The digitizers in {root_filename} have zero events. Skipping this file.")
         return
     print(f"All {n_channels} active channels have {n_events} events")
-    for i, c in enumerate(channels):
+    i = 0
+    optional_branches = ["timeStamp", "triggerTime"]
+    for i, (d, c) in enumerate((d, c) for d, channels in digitizer_channels.items() for c in channels):
         channel_name = cm.channel_names[i]
-        print(f"Processing {c} into {channel_name}")
-        for batch, report in run_file[c].iterate(step_size="100 MB", report=True):
+        print(f"Processing {d}/{c} into {channel_name}")
+        branches = [c] + [b for b in optional_branches if b in run_file[d].keys()]
+        for batch, report in run_file[d].iterate(branches, step_size="100 MB", report=True):
             print(f"... events {report.tree_entry_start} to {report.tree_entry_stop}")
             config_args = {
                 "threshold": config["Thresholds"][i],
@@ -80,7 +86,9 @@ def process_file(root_filename, config, output_file):
                 "voltage_scale": config["VoltageScale"],
                 "time_offset": config["TimeOffset"][i],
             }
-            process_batch(batch[batch.fields[0]], channel_name, output_file, config_args)
+            waveforms = batch[c]
+            extra_branches = {b: batch[b] for b in branches if b != c}
+            process_batch(waveforms, extra_branches, channel_name, output_file, config_args)
 
     run_number = int(root_filename[-11:-5])
     print(f"Saving event info for run {run_number}")
@@ -92,11 +100,11 @@ def process_file(root_filename, config, output_file):
     run_file.close()
 
 
-def process_batch(waveforms, channel, output_file, config_args):
+def process_batch(waveforms, extra_branches, channel, output_file, config_args):
     waveform_analysis = WaveformAnalysis(waveforms, **config_args)
     waveform_analysis.run_analysis()
-    pedestals = waveform_analysis.pedestals.squeeze()
-    pedestal_sigmas = waveform_analysis.pedestal_sigmas.squeeze()
+    pedestals = ak.Array(waveform_analysis.pedestals.squeeze())
+    pedestal_sigmas = ak.Array(waveform_analysis.pedestal_sigmas.squeeze())
     peak_voltages = waveform_analysis.pulse_peak_voltages
     peak_times = waveform_analysis.pulse_peak_times
     signal_times = waveform_analysis.pulse_signal_times
@@ -109,11 +117,12 @@ def process_batch(waveforms, channel, output_file, config_args):
 
     branches = {"Pedestal": pedestals,
                 "PedestalSigma": pedestal_sigmas,
-                "Peaks": peaks}
+                "Peaks": peaks,
+                **extra_branches}
 
     print(f"... writing {channel} to {output_file.file_path}")
     if channel not in output_file.keys():
-        branch_types = {"Pedestal": "float64", "PedestalSigma": "float64", "Peaks": peaks.type}
+        branch_types = {name: branch.type for name, branch in branches.items()}
         output_file.mktree(channel, branch_types, counter_name=(lambda s: "nPeaks"), field_name=(lambda s, f: f))
     output_file[channel].extend(branches)
 
